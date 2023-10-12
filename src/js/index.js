128f14581a2d03fcde5a3ee93049652fe23f8297
Rafaela Wolf <rafaelawolf52@gmail.com>
2023-10-12 17:20:54 -0300

    fazendo mudanças e adicionando o js

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,14 @@
-const campoObrigatorio = document.getElementsByClassName('.texto-vermelho');
+const botaoEnviar = document.querySelector('.botao');
+
+const msgCampoObrigatorio = document.getElementsByClassName('.texto-vermelho');
+
+const nome = document.getElementById("nome").value;
+
+botaoEnviar.addEventListener('click', function () {
+        if (nome.value === "") {
+           alert("oi");
+    }
+})
+
+
+    
