03ac4139d3088bee34c85cfbd2fdb44e3fd2ab33
Rafaela Wolf <rafaelawolf52@gmail.com>
2023-10-13 17:25:36 -0300

    modificações

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,54 @@
 const botaoEnviar = document.querySelector('.botao');
 
-const msgCampoObrigatorio = document.getElementsByClassName('.texto-vermelho');
+const inputs = ["nome", "email", "celular", "mensagem"];
 
-const nome = document.getElementById("nome").value;
+//const nome = document.getElementById('nome');
 
-botaoEnviar.addEventListener('click', function () {
-        if (nome.value === "") {
-           alert("oi");
-    }
+//const email = document.getElementById('email');
+
+//const celular = document.getElementById('celular');
+
+//const mensagem = document.getElementById('mensagem');
+
+botaoEnviar.addEventListener('click', () => {
+    inputs.forEach(function (input) {
+        if (input.value === "") {
+            input.classList.add("invalidado");
+        } else {
+            input.classList.add("validado");
+        }
+    });
 })
 
+// botaoEnviar.addEventListener('click', () => {
+//     if (nome.value === "") {
+//         nome.classList.add("invalidado");
+//     } else {
+//         nome.classList.add("validado");
+//     }
+// })
 
+// botaoEnviar.addEventListener('click', () => {
+//     if (email.value === "") {
+//         email.classList.add("invalidado");
+//     } else {
+//         email.classList.add("validado");
+//     }
+// })
+
+// botaoEnviar.addEventListener('click', () => {
+//     if (celular.value === "") {
+//         celular.classList.add("invalidado");
+//     } else {
+//         celular.classList.add("validado");
+//     }
+// })
+
+// botaoEnviar.addEventListener('click', () => {
+//     if (mensagem.value === "") {
+//         mensagem.classList.add("invalidado");
+//     } else {
+//         mensagem.classList.add("validado");
+//     }
+// })
     
